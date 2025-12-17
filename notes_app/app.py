036cd3408ff147ffa25dce453b6d1915429c53b7
moonnnluv <ale.reyesd@duocuc.cl>
2025-12-17 00:42:58 -0300

    creación home y note form

--- notes_app/app.py
+++ notes_app/app.py
@@ -1,10 +1,12 @@
-from flask import Flask, request, jsonify
+from flask import Flask, redirect, request, jsonify, render_template, url_for, redirect
 
 app = Flask(__name__)
 
 @app.route("/")
-def hola():
-    return "Hola Mundo!"
+def home():
+    role = "admin"
+    notes = ["Nota 1", "Nota 2", "Nota 3"]
+    return render_template("home.html", role=role, notes=notes)
 
 
 @app.route("/about")
@@ -21,7 +23,20 @@ def contact():
 def api_info():
     data = {
         "app": "Notes App",
-        "version": 1.0,
+        "version": "1.0.0",
         "author": "Ale"
     } 
-    return jsonify(data)
+    return jsonify(data), 200
+
+@app.route("/confirmacion")
+def confirmation():
+    return "Prueba"
+
+@app.route("/crear-nota", methods=['POST', 'GET'])
+def create_note():
+    if request.method == 'POST':
+        nota = request.form.get('nota', "No encontrada")
+        return redirect(
+            url_for("confirmation", nota=nota )
+        )
+    return render_template("note_form.html")
